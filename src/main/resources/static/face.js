9afe1a6d1e3d03b90b8722eb5ab34e671a80b358
sejelly <seyoung0584@naver.com>
2022-10-31 17:24:08 +0900

    webcam 캡쳐 사진 저장까지

--- src/main/resources/static/face.js
+++ src/main/resources/static/face.js
@@ -1,1 +1,43 @@
-const webcamElement = document.getElementById('webcam');
+var video = document.querySelector("#videoElement");
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch(function (err0r) {
+            console.log("Something went wrong!");
+        });
+}
+
+const captureDiv = document.getElementById('captureDiv');
+function takePhoto(){
+    makeDivToImageFile(captureDiv);
+}
+function saveAs(url, fileName) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
+
+function makeDivToImageFile(captureDiv) {
+
+    html2canvas(captureDiv, {
+        allowTaint: true,
+        useCORS: true,
+
+        /* 아래 3 속성이 canvas의 크기를 정해준다. */
+        width: captureDiv.offsetWidth,
+        height: captureDiv.offsetHeight,
+        scale: 1
+    }).then(function (canvas) {
+        const imageURL = canvas.toDataURL('image/jpeg');
+        saveAs(imageURL, 'new file.jpg');
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+}
